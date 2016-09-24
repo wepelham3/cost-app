6a36bbe87c8c61533d4a52a6cefce84c0564aa2a
William Pelham <wpelham@sundevils.asu.edu>
2016-09-24 16:35:58 -0700

    change UI to navbarPage

--- ui.R
+++ ui.R
@@ -8,29 +8,27 @@ options(shiny.trace=TRUE)
 # --------------------------------------------------------------------
 
 
-shinyUI(fluidPage(
-
-  #use shiny js 
-  shinyjs::useShinyjs(),
+shinyUI(
+  navbarPage("Cost of Treatment Calculator",
+             
+             windowTitle = "Cost of Treatment Calculator",
+             selected = "About the app.",
+             
+             # fix the navbar to always show and provide padding so it doesn't overlay the panels
+             position = "fixed-top",
+             tags$style(type="text/css", "body {padding-top: 70px;}"),
+             
+             shinyjs::useShinyjs(),
+             
+             theme = "flatly.css",
+             tags$head(tags$style('nav .container:first-child {margin-left:10px; width: 100%;}')),
+             tags$head(tags$style(HTML("h1 {color: #e6b800;}"))),
+             
+             tabPanel(title = "About the app.", "spiel about the app"),
+             tabPanel(title = "Watch a 5-minute intro video.", "youtube embed"),
+             tabPanel(title = "Use the calculator.", source("use-the-calculator.R")),
+             tabPanel(title = "Look up prices and wages.", source("look-up-prices.R")),
+             tabPanel(title = "Contact us.", "github page, email, etc.")
   
-  theme = "flatly.css",
-  
-  tags$head(
-    tags$style(HTML("
-      
-      h1 {
-        color: #e6b800;
-      }
-
-    "))
-  ),
-
-  # Application title
-  titlePanel(tags$h1("Cost of Treatment Calculator"), windowTitle = "Cost of Treatment Calculator"),
-  
-  tags$hr(),
-  
-  tabsetPanel(tabPanel(title = "Use the calculator.", source("use-the-calculator.R")),
-              tabPanel(title = "Look up prices.", source("look-up-prices.R")))
-  )
+             )
   )
