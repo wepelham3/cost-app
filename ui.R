b62acf7a2f83a1315e63f791493ee3066eed9433
William Pelham <wpelham@sundevils.asu.edu>
2016-09-24 16:56:25 -0700

    edit a navbar label

--- ui.R
+++ ui.R
@@ -25,7 +25,7 @@ shinyUI(
              tags$head(tags$style(HTML("h1 {color: #e6b800;}"))),
              
              tabPanel(title = "About the app.", "spiel about the app"),
-             tabPanel(title = "Watch a 5-minute intro video.", "youtube embed"),
+             tabPanel(title = "Watch a 5-minute video to get started.", "youtube embed"),
              tabPanel(title = "Use the calculator.", source("use-the-calculator.R")),
              tabPanel(title = "Look up prices and wages.", source("look-up-prices.R")),
              tabPanel(title = "Contact us.", "github page, email, etc.")
