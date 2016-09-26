561deea2b28e0dc199488a745dd0560a727080ed
William Pelham <wpelham@sundevils.asu.edu>
2016-09-26 13:59:01 -0700

    split all navbar tabs UI into their own file

---
+++ ui-navbar-tabs/contact-us.R
@@ -0,0 +1,1 @@
+"github page, email, etc."
--- ui.R
+++ ui.R
@@ -24,11 +24,23 @@ shinyUI(
              tags$head(tags$style('nav .container:first-child {margin-left:10px; width: 100%;}')),
              tags$head(tags$style(HTML("h1 {color: #e6b800;}"))),
              
-             tabPanel(title = "About the app.", "spiel about the app"),
-             tabPanel(title = "Watch a 5-minute video to get started.", "youtube embed"),
-             tabPanel(title = "Use the calculator.", source("use-the-calculator.R")$value),
-             tabPanel(title = "Look up prices and wages.", source("look-up-prices.R")$value),
-             tabPanel(title = "Contact us.", "github page, email, etc.")
+             tabPanel(title = "About the app.",
+                      source("ui-navbar-tabs/about-the-app.R")$value),
+             
+             tabPanel(title = "Watch a 5-minute video to get started.",
+                      source("ui-navbar-tabs/watch-a-video.R")$value),
+             
+             tabPanel(title = "Use the calculator.",
+                      source("ui-navbar-tabs/use-the-calculator.R")$value),
+             
+             tabPanel(title = "Compare protocols.",
+                      source("ui-navbar-tabs/compare-protocols.R")$value),
+             
+             tabPanel(title = "Look up prices and wages.",
+                      source("ui-navbar-tabs/look-up-prices.R")$value),
+             
+             tabPanel(title = "Contact us.",
+                      source("ui-navbar-tabs/contact-us.R")$value)
   
              )
   )
