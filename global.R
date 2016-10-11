fa25c875077f0a4cf1e7528c3020d153f21d0d26
wepelham3 <wp8000@gmail.com>
2016-10-10 20:39:57 -0700

    reorganize functions folder per Gladys's email

--- global.R
+++ global.R
@@ -7,5 +7,5 @@ df.meds <- read_csv("data/data-meds.csv", col_names = TRUE)
 df.comps <- read_csv("data/data-comps.csv", col_names = TRUE)
 
 # source all global functions
-list.files("global-functions/", full.names = TRUE) %>%
+list.files("global-functions/", recursive = TRUE, full.names = TRUE) %>%
   lapply(source)
